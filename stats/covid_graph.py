89634975e12f41c8018a60afce15f44c1d23a3d7
kinonumba9 <kinolavish9@icloud.com>
2020-05-01 09:21:34 -0400

    update

--- stats/covid_graph.py
+++ stats/covid_graph.py
@@ -1,21 +1,27 @@
 import csv
 import matplotlib.pyplot as plt
 
+
 dates=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,
    35,36,37,38,39,40,41,42,43,44]
-cases=[1,1,1,2,3,5,5,5,6,7,8,11,11,11,12,12,12,12,12,13,13,14,15,15,15,15,25,25,25,27,30,30,30,43,
-   45,60,60,65,70,88,104,125,161,228]
+cases=[88,104,125,161,228,311,428,547,748,1018,1263,1668,2224,2898,3600,4507,5905,8345,12384,17990,
+       24511,33043,43477,53892,68489,85497,102729,123581,142194,163589,188049,214711,244034,276501,
+       311536,336711,397748,429316,463684,49614,528397,555327,580853,607285]
+deaths = [3,6,10,12,12,15,19,22,26,31,37,43,50,60,68,91,116,157,203,343,435,550,731,989,1275,1648,
+          2134,2487,3068,3911,4841,5952,7121,8499,9654,10958,12957,14803,16673,18710,10958,12957,
+          14803,16673]
 #plt.plot(dates,cases)
 
 plt.style.use('seaborn')
 fig, ax = plt.subplots()
 ax.plot(dates, cases, c='red')
+ax.plot(dates, deaths, c='green')
 
-# Format plot.
-plt.title("Daily covid cases - 2020", fontsize=24)
+## Format plot.
+plt.title("Daily covid cases and deaths - 2020", fontsize=24)
 plt.xlabel('', fontsize=16)
 fig.autofmt_xdate()
-plt.ylabel("cases", fontsize=16)
+plt.ylabel("cases(red) and death(green)", fontsize=16)
 plt.xlabel("days", fontsize=16)
 
 plt.tick_params(axis='both', which='major', labelsize=16)
